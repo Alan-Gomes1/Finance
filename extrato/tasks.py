296b287a64a24091cd91f5a80e843a8b78ecf46d
Alan <alan.gomes.ag28@gmail.com>
2025-03-31 12:36:18 -0300

    feat: add send_email_task

---
+++ extrato/tasks.py
@@ -0,0 +1,21 @@
+from celery import shared_task
+from django.core.mail import send_mail
+from django.conf import settings
+
+
+@shared_task
+def send_email_task(subject, message, recipient_list):
+    """Task de envio de emails
+
+    Args:
+        subject (str): Assunto
+        message (str): Corpo do email
+        recipient_list (str): Destinatario
+    """
+    send_mail(
+        subject=subject,
+        message=message,
+        from_email=settings.EMAIL_HOST_USER,
+        recipient_list=recipient_list,
+        fail_silently=False
+    )
